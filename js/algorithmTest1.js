a98c1b37769ee595b94dadd65899d64845d555a9
TaylorBett <taylor.bett@hotmail.com>
2014-02-05 21:17:41 +1000

    Added JS and my user input form to 'signin.html' page, removed old form.
    Tried to create a button that would call calcBAC but it is coming up as
    NaN (not a number), so will ask James tomorrow.
    
    Sorry, thought I was working on my own branch but for some reason
    gitHub won’t let me push changes to that, only to master…
    
    Anyway, if you could have a look at styling this when you’re ready and
    letting me know if you need any clarification etc, these should be the
    correct input fields now. Don’t worry about the p tags that are
    confirming the input with the user, they are just there for js testing
    at the moment.
    
    Sorry about ruining the nice layout, haven’t written much CSS for this,
    just functionality

---
+++ js/algorithmTest1.js
@@ -0,0 +1,70 @@
+var update = document.getElementById("myStat2");
+
+var c=document.getElementById("myCanvas");
+            var ctx=c.getContext("2d");
+
+var userName = "Your name here";
+var weight = 60;
+var gender = "unknown";
+var genderConstant = 6.8;
+
+var hoursTotal = 4;		//hours since session start
+var SDTotal = 3;		//total standard drinks consumed
+
+var BAC = 0.00;			//Blood Alcohol Content
+var SoberIn = 10;		//estimated time till sober
+var SoberTime = 10;		//estimated time when sober
+
+//awaiting data
+var beerArray = [1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8];	
+var wineArray = [1,1.1,1.2,];
+var spiritsArray = [1,1.1,1.2,];
+
+
+function getUserInput(){
+	userName = document.getElementById("userName").value;
+	weight = document.getElementById("userWeight2").value;
+	parseInt(weight);
+	if(document.getElementById('maleGender').checked) {
+		gender = "male";
+		genderConstant = 6.8;
+	}
+	else if(document.getElementById('femaleGender').checked) {
+		gender = "female";
+		genderConstant = 5.5;
+	}
+	document.getElementById("greeting").innerHTML = "Hi there, " + userName + "!";
+	document.getElementById("dataCheck").innerHTML = "So, just to double check, you're " + gender + " and you weigh about " + weight + " kgs, right?";
+}
+
+	//screen shake - how to do it?
+
+function calcBAC(){
+	BAC = ((SDTotal * 10)-(hoursTotal * 7.5))/(weight * genderConstant);
+	parseFloat(BAC);
+	document.getElementById("yourBAC").innerHTML = "Your BAC is " + BAC;
+	
+
+	console.log(BAC);
+	//document.getElementById("myStat2").setAttribute("data-text",BAC);
+	//document.getElementById("myStat2").setAttribute("data-percent",(BAC*1000));
+            /*ctx.beginPath();
+            ctx.arc(150,150,120,0,((BAC/0.1)*2)*Math.PI);
+            ctx.stroke();
+            ctx.lineWidth = 32.5;*/
+}
+
+calcBAC();
+setInterval(calcBAC,500);
+
+
+function calcSoberCountdown(){
+	SoberIn = (SDTotal*10)/7.5;		   //need to figure out way to use minutes
+}
+
+function calcSoberTime(){
+	SoberTime = (currentTime+SoberIn); //need to figure out way to use minutes
+}
+
+
+

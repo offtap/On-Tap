7fda11bf5f5b875587ab037dc37ebf7f99c5f746
TaylorBett <taylor.bett@hotmail.com>
2014-02-06 15:51:53 +1000

    Fixed BAC = NaN problem + changed weight to slider
    BAC button fixed, now presents correct decimal data, also SoberIn time
    function works and presents user with hours until 0.00 BAC. This will
    be changed from decimal to hours/mins in future.
    
    Reverted weight input to range slider rather than number input for UI
    reasons (needs styling).

--- js/algorithmTest1.js
+++ js/algorithmTest1.js
@@ -1,7 +1,10 @@
-var update = document.getElementById("myStat2");
+alert("JS loaded");
 
-var c=document.getElementById("myCanvas");
-            var ctx=c.getContext("2d");
+
+//var update = document.getElementById("myStat2");
+
+//var c=document.getElementById("myCanvas");
+            //var ctx=c.getContext("2d");
 
 var userName = "Your name here";
 var weight = 60;
@@ -9,7 +12,7 @@ var gender = "unknown";
 var genderConstant = 6.8;
 
 var hoursTotal = 4;		//hours since session start
-var SDTotal = 3;		//total standard drinks consumed
+var SDTotal = 4;		//total standard drinks consumed
 
 var BAC = 0.00;			//Blood Alcohol Content
 var SoberIn = 10;		//estimated time till sober
@@ -20,16 +23,16 @@ var beerArray = [1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8];
 var wineArray = [1,1.1,1.2,];
 var spiritsArray = [1,1.1,1.2,];
 
+//calcBAC();
 
 function getUserInput(){
 	userName = document.getElementById("userName").value;
 	weight = document.getElementById("userWeight2").value;
-	parseInt(weight);
-	if(document.getElementById('maleGender').checked) {
+	if(document.getElementById("maleGender").checked) {
 		gender = "male";
 		genderConstant = 6.8;
 	}
-	else if(document.getElementById('femaleGender').checked) {
+	else if(document.getElementById("femaleGender").checked) {
 		gender = "female";
 		genderConstant = 5.5;
 	}
@@ -41,8 +44,10 @@ function getUserInput(){
 
 function calcBAC(){
 	BAC = ((SDTotal * 10)-(hoursTotal * 7.5))/(weight * genderConstant);
-	parseFloat(BAC);
+	alert(BAC);
 	document.getElementById("yourBAC").innerHTML = "Your BAC is " + BAC;
+	calcSoberCountdown();
+	document.getElementById("SoberIn").innerHTML = "You will be sober in " + SoberIn + " hours.";
 	
 
 	console.log(BAC);
@@ -54,17 +59,16 @@ function calcBAC(){
             ctx.lineWidth = 32.5;*/
 }
 
-calcBAC();
-setInterval(calcBAC,500);
-
 
 function calcSoberCountdown(){
-	SoberIn = (SDTotal*10)/7.5;		   //need to figure out way to use minutes
+	SoberIn = (SDTotal*10)/7.5;
+			   //need to figure out way to use minutes
 }
 
 function calcSoberTime(){
 	SoberTime = (currentTime+SoberIn); //need to figure out way to use minutes
 }
+
 
 
 

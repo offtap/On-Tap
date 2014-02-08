16f0256775014256a72b3e356bef55773815b866
TaylorBett <taylor.bett@hotmail.com>
2014-02-08 14:47:05 +1000

    Added functional drink adding test to index.html page
    Test is just functional, minimal styling applied. It uses the same
    methods we will hopefully apply for the final product.
    
    Please note that as the index.html and sign-in.html pages are currently
    separate, the user inputs form sign-in do not yet carry over to the
    tracker page, meaning that it does not yet use the user weight and
    gender, but the presets of 60kg and male.
    
    The test currently adds 1 hour per drink entered, and the drink types
    do not reflect real SD numbers but rather just values of 1, 2 and 3 SDs
    for testing purposes. Feel free to try it out and to check out the
    code.
    
    I have also added the ability to change the previous decimal returns
    for SoberIn time to hours and mins :)

--- js/algorithmTest1.js
+++ js/algorithmTest1.js
@@ -1,6 +1,6 @@
 alert("JS loaded");
 
-
+//ignore this code, it is for circlify/manual circle tests
 //var update = document.getElementById("myStat2");
 
 //var c=document.getElementById("myCanvas");
@@ -11,19 +11,26 @@ var weight = 60;
 var gender = "unknown";
 var genderConstant = 6.8;
 
-var hoursTotal = 4;		//hours since session start
-var SDTotal = 4;		//total standard drinks consumed
+var hoursTotal = 0.00;		//hours since session start
+var SDTotal = 0.00;		//total standard drinks consumed
 
 var BAC = 0.00;			//Blood Alcohol Content
-var SoberIn = 10;		//estimated time till sober
-var SoberTime = 10;		//estimated time when sober
+
+var SoberInTotal = 0.00;	//estimated total time till sober
+var SoberInDecimal = 0.00;		//multiple vars needed to calc hours + mins
+var SoberInHours = 0.00;
+var SoberInMins = 0.00;
+
+var SoberTime = 0.00;		//estimated time when sober
 
 //awaiting data
 var beerArray = [1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8];	
 var wineArray = [1,1.1,1.2,];
 var spiritsArray = [1,1.1,1.2,];
 
-//calcBAC();
+var drinksArrayTest = [1,2,3]; //this is to test the method we will use for all drinks
+
+var newDrinkSD = 0.00; //number of standard drinks added to SDTotal when new drink entered
 
 function getUserInput(){
 	userName = document.getElementById("userName").value;
@@ -40,17 +47,29 @@ function getUserInput(){
 	document.getElementById("dataCheck").innerHTML = "So, just to double check, you're " + gender + " and you weigh about " + weight + " kgs, right?";
 }
 
-	//screen shake - how to do it?
+function addNewDrink() {
+	hoursTotal = hoursTotal+1;
+	if(document.getElementById("drinkType:Beer").checked) {
+		newDrinkSD = drinksArrayTest[0];
+	}
+	else if(document.getElementById("drinkType:Wine").checked) {
+		newDrinkSD = drinksArrayTest[1];
+	}
+	else if(document.getElementById("drinkType:Spirit").checked) {
+		newDrinkSD = drinksArrayTest[2];
+	}
+	SDTotal = SDTotal + newDrinkSD;
+	calcBAC();
+	calcSoberIn();
+	document.getElementById("BACCounter").innerHTML = BAC;
+	document.getElementById("SoberInCounter").innerHTML = SoberInHours + " hrs " + SoberInMins + " mins";
+	document.getElementById("drinkInputTest").reset();
+}
 
 function calcBAC(){
 	BAC = ((SDTotal * 10)-(hoursTotal * 7.5))/(weight * genderConstant);
 	alert(BAC);
-	document.getElementById("yourBAC").innerHTML = "Your BAC is " + BAC;
-	calcSoberCountdown();
-	document.getElementById("SoberIn").innerHTML = "You will be sober in " + SoberIn + " hours.";
-	
-
-	console.log(BAC);
+	//circle code test below
 	//document.getElementById("myStat2").setAttribute("data-text",BAC);
 	//document.getElementById("myStat2").setAttribute("data-percent",(BAC*1000));
             /*ctx.beginPath();
@@ -59,10 +78,11 @@ function calcBAC(){
             ctx.lineWidth = 32.5;*/
 }
 
-
-function calcSoberCountdown(){
-	SoberIn = (SDTotal*10)/7.5;
-			   //need to figure out way to use minutes
+function calcSoberIn(){
+	SoberInTotal = (SDTotal*10)/7.5;
+	SoberInDecimal = SoberInTotal%1;
+	SoberInHours = SoberInTotal-SoberInDecimal;
+	SoberInMins = Math.round(SoberInDecimal*60);
 }
 
 function calcSoberTime(){

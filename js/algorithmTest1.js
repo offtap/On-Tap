f168e1c4eaf747042c96d47e650b392a3213789a
TaylorBett <taylor.bett@hotmail.com>
2014-02-11 01:10:42 +1000

    Form validation, Profile pic input test, Circle visualiser testing page
    Form validation built into sign-in page, testing input type=“file” to
    see if we want to look at users entering a photo for a profile pic type
    of thing.
    
    Also a separate page for testing the circle animation has been started,
    but still needs work as it is just seeing if the animation method works
    currently.

--- js/algorithmTest1.js
+++ js/algorithmTest1.js
@@ -1,11 +1,8 @@
 alert("JS loaded");
 
-//ignore this code, it is for circlify/manual circle tests
-//var update = document.getElementById("myStat2");
-
-//var c=document.getElementById("myCanvas");
-            //var ctx=c.getContext("2d");
-
+//var c = document.getElementById("myCanvas");
+//var ctx = c.getContext("2d");
+                    
 var userName = "Your name here";
 var weight = 60;
 var gender = "unknown";
@@ -14,7 +11,7 @@ var genderConstant = 6.8;
 var hoursTotal = 0.00;		//hours since session start
 var SDTotal = 0.00;		//total standard drinks consumed
 
-var BAC = 0.00;			//Blood Alcohol Content
+var BAC = 0.05;			//Blood Alcohol Content
 
 var SoberInTotal = 0.00;	//estimated total time till sober
 var SoberInDecimal = 0.00;		//multiple vars needed to calc hours + mins
@@ -24,42 +21,53 @@ var SoberInMins = 0.00;
 var SoberTime = 0.00;		//estimated time when sober
 
 //awaiting data
-var beerArray = [1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8];	
-var wineArray = [1,1.1,1.2,];
-var spiritsArray = [1,1.1,1.2,];
-
-var drinksArrayTest = [1,2,3]; //this is to test the method we will use for all drinks
+var beerArray = [0.6,0.9,1.2,0.8,1.2,1.6,1.1,1.6,2.2];
+var wineArray = [1,1.5,0.9,1.4,1.4];
+var spiritArray = [0.5,1,2];
 
 var newDrinkSD = 0.00; //number of standard drinks added to SDTotal when new drink entered
 
 function getUserInput(){
-	userName = document.getElementById("userName").value;
-	weight = document.getElementById("userWeight2").value;
-	if(document.getElementById("maleGender").checked) {
-		gender = "male";
-		genderConstant = 6.8;
+	if(document.getElementById("userName").value.length!=0){
+		if(!(document.getElementById("maleGender").checked)&&(!(document.getElementById("femaleGender").checked))){
+			alert("Please select your gender");
+		}
+		else{
+			userName = document.getElementById("userName").value;
+			weight = document.getElementById("userWeight").value;
+			if(document.getElementById("maleGender").checked) {
+				gender = "male";
+				genderConstant = 6.8;
+			}
+			else if(document.getElementById("femaleGender").checked) {
+				gender = "female";
+				genderConstant = 5.5;
+			}
+			document.getElementById("greeting").innerHTML = "Hi there, " + userName + "!";
+			document.getElementById("dataCheck").innerHTML = "So, just to double check, you're " + gender + " and you weigh about " + weight + " kgs, right?";
+			}
+		}
+	else{
+		alert("Please enter a valid username");
 	}
-	else if(document.getElementById("femaleGender").checked) {
-		gender = "female";
-		genderConstant = 5.5;
-	}
-	document.getElementById("greeting").innerHTML = "Hi there, " + userName + "!";
-	document.getElementById("dataCheck").innerHTML = "So, just to double check, you're " + gender + " and you weigh about " + weight + " kgs, right?";
 }
 
 function addNewDrink() {
-	hoursTotal = hoursTotal+1;
+	hoursTotal = hoursTotal + 1;
 	if(document.getElementById("drinkType:Beer").checked) {
-		newDrinkSD = drinksArrayTest[0];
+		newDrinkSD = beerArray[0];
 	}
 	else if(document.getElementById("drinkType:Wine").checked) {
-		newDrinkSD = drinksArrayTest[1];
+		newDrinkSD = wineArray[1];
 	}
 	else if(document.getElementById("drinkType:Spirit").checked) {
-		newDrinkSD = drinksArrayTest[2];
+		newDrinkSD = spiritArray[1];
 	}
 	SDTotal = SDTotal + newDrinkSD;
 	calcBAC();
+	if(BAC<0.00){
+		BAC = 0.00;
+	}
 	calcSoberIn();
 	document.getElementById("BACCounter").innerHTML = BAC;
 	document.getElementById("SoberInCounter").innerHTML = SoberInHours + " hrs " + SoberInMins + " mins";
@@ -67,15 +75,17 @@ function addNewDrink() {
 }
 
 function calcBAC(){
-	BAC = ((SDTotal * 10)-(hoursTotal * 7.5))/(weight * genderConstant);
+	BAC = ((SDTotal * 10)-(hoursTotal * 7.5))/(weight * localStorage.genderConstant);
 	alert(BAC);
-	//circle code test below
-	//document.getElementById("myStat2").setAttribute("data-text",BAC);
-	//document.getElementById("myStat2").setAttribute("data-percent",(BAC*1000));
-            /*ctx.beginPath();
-            ctx.arc(150,150,120,0,((BAC/0.1)*2)*Math.PI);
-            ctx.stroke();
-            ctx.lineWidth = 32.5;*/
+	//drawCircle();
+}
+
+function drawCircle(){
+	ctx.beginPath();
+    ctx.arc(125,125,100,-0.5*Math.PI,0.5*(2*Math.PI));
+    ctx.lineWidth=25;
+    ctx.strokeStyle= '#3498db';
+    ctx.stroke();
 }
 
 function calcSoberIn(){
@@ -88,7 +98,6 @@ function calcSoberIn(){
 function calcSoberTime(){
 	SoberTime = (currentTime+SoberIn); //need to figure out way to use minutes
 }
-
 
 
 

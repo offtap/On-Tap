8261c6392a119b66e9507dc74d15ee9645965423
benhare <benahare@gmail.com>
2014-02-11 22:14:39 +1000

    Added FAQ Jquery Script
    *css styling for FAQ
    *js script added to js folder

---
+++ js/scripts.js
@@ -0,0 +1,99 @@
+jQuery(document).ready(function ($) {
+
+
+    $(window).stellar();
+
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+
+
+    slide.waypoint(function (event, direction) {
+
+        dataslide = $(this).attr('data-slide');
+
+        if (direction === 'down') {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+        }
+        else {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+
+    });
+ 
+    mywindow.scroll(function () {
+        if (mywindow.scrollTop() == 0) {
+            $('.navigation li[data-slide="1"]').addClass('active');
+            $('.navigation li[data-slide="2"]').removeClass('active');
+        }
+    });
+
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+
+    });
+
+
+});
+
+
+$(document).ready(function(){
+
+    // hide #back-top first
+    $("#back-top").hide();
+    
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 300) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+});
+
+/**
+ * Hides all other answers and shows the answer to clicked question
+ *
+ * @param id CSS  of answer
+ */
+function showAnswer(id) {
+    var questions = ['london', 'south-west', 'south-central', 'north', 'midlands'];
+    for(var i = 0; i < questions.length; i++)
+        if(id != questions[i]) $('#' + questions[i]).hide();
+
+    if($(id).css('display') == 'block')
+        $(id).hide();
+    else
+        $(id).show();
+
+}

1a5002cf2d9f915d35bc8d432500192aa18ec0f9
TaylorBett <taylor.bett@hotmail.com>
2014-02-23 12:51:14 +1000

    Added refreshing BAC Counter to visual display

--- js/algorithmTest1.js
+++ js/algorithmTest1.js
@@ -95,6 +95,9 @@ function addNewDrink() {
 	}
 	drawNewDrink();
 	calcSoberIn();
+	ctx.clearRect(60,75,140,100);
+	ctx.font = "50px Arial";
+	ctx.fillText(newBAC.toFixed(3),60,142);
 	document.getElementById("BACCounter").innerHTML = newBAC;
 	document.getElementById("SoberInCounter").innerHTML = SoberInHours + " hrs " + SoberInMins + " mins";
 	document.getElementById("drinkInputTest").reset();

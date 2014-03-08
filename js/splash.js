2a7e66153912f37a8404de74302f5e645449e6e0
benhare <benahare@gmail.com>
2014-03-08 19:40:50 +1000

    Styling

--- js/splash.js
+++ js/splash.js
@@ -2,4 +2,7 @@ function removeSplash(){
 	document.getElementById('container').style.display='none';
 	document.getElementById('b').style.display='block';
 	document.getElementById('menuLink').style.display='block';
+
+	document.getElementById('b').style.display='none';
+	document.getElementById('a').style.display='block';
 }
